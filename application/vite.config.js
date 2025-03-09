01e125f4af0b80599a186be5a41f92a48f2d5639
Eugeny Zaiko <e.zaiko@syberry.com>
2025-03-09 16:43:30 +0300

    Fix: improved together using docker with vite

--- application/vite.config.js
+++ application/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+    },
+
     plugins: [
         laravel({
             input: [
